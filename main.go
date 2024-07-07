461be896640e192186941742ec31266ae8e88eb8
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2024-07-07 18:48:08 +0530

    sqlc and goose setup

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 
+	"github.com/Qu-Ack/RSS_Blog_Aggregator/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	mux := http.NewServeMux()
 	err := godotenv.Load(".env")
 	port := os.Getenv("PORT")
+	dbURL := os.Getenv("DB_STRING")
 	if err != nil {
 		fmt.Println("Error While Loading Env Var")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+
+	dbQueries := database.New(db)
+
+	apiconfig := &apiConfig{
+		DB: dbQueries,
 	}
 
 	server := http.Server{
